a263f997b78198d2c4243da25b99695a1fa00fbd
ltyle <45313165+ltyle@users.noreply.github.com>
2019-05-31 16:55:47 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,7 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|57021-8', 'http://loinc.org|55284-4']
+                              'http://loinc.org|8867-4', 'http://loinc.org|55284-4']
                       }
                     }
                   });
